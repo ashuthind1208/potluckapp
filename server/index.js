cb98faea6baf4656ce921bba73539d6398406c7b
Aashoodeep Singh <ashu.thind@gmail.com>
2023-11-21 20:55:54 -0500

    changes to index file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import Express  from "express";
 import { pathRouter } from "./src/routes/path.js";
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose';
+//require('dotenv').config()
 
 const app = Express();
 
@@ -10,9 +11,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/_api',pathRouter);
 
-mongoose.connect('mongodb+srv://ashuthind:Admin786@cluster0.dq7uy.azure.mongodb.net/potluck')
+console.log("path "+)
+let dotenv = require('dotenv').config({path:__dirname+'/.env'})
+console.log(dotenv);
+
+
+mongoose.connect(process.env['MONGODB_URL'])    
 .then(console.log('mongoose connected'))
 
 
 
-app.listen(3000,()=>{console.log('server started on 3000')})
+app.listen(3000,()=>{console.log('server started on 3000',{path:__dirname})})

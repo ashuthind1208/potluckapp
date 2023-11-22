fe714606009b1ff78861b29069ed506376edaa9c
Aashoodeep Singh <ashu.thind@gmail.com>
2023-11-21 21:15:58 -0500

    made changes to index file

---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+import Express  from "express";
+import { pathRouter } from "./routes/path.js";
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose';
+
+const app = Express();
+
+app.use(Express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/_api',pathRouter);
+
+
+
+
+mongoose.connect("mongodb+srv://ashuthind:Admin786@cluster0.dq7uy.azure.mongodb.net/potluck")    
+.then(console.log('mongoose connected'))
+
+
+
+app.listen(3000,()=>{console.log('server started on 6000')})

12c73bf11dee6e5af1c53450bf027fd7bd6b3925
Aashoodeep Singh <ashu.thind@gmail.com>
2023-11-21 22:12:03 -0500

    mongose connection

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,9 @@ import Express  from "express";
 import { pathRouter } from "./routes/path.js";
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import path from 'path';
+
 
 const app = Express();
 
@@ -11,11 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/_api',pathRouter);
 
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL,console.log('mongoose connected'))
 
 
-mongoose.connect("mongodb+srv://ashuthind:Admin786@cluster0.dq7uy.azure.mongodb.net/potluck")    
-.then(console.log('mongoose connected'))
 
-
-
-app.listen(3000,()=>{console.log('server started on 6000')})
+app.listen(3001,()=>{console.log('server started on 3001')})
